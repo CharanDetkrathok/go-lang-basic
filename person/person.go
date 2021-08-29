84a61780de87954c3dcbaa42872ef3771479913f
Charan Detkrathok <charandetkrathok@gmail.com>
2021-08-29 16:52:45 +0700

    basic go lang version 0.2

--- person/person.go
+++ person/person.go
@@ -12,6 +12,8 @@ func (p Person) GetName() string {
 	return p.name
 }
 
+// ใช้ * เพื่ออ้างอิงถึง ตำแหน้งเก็บข้อมูลของ struct นั้นๆที่ถูกสร้าง และเมื่อเรียก receiver function ที่ต้องการ set value ถ้าไม่ใช้ * เวลา assignment value มาที่ func (p Person) เมื่อต้องการเรียกดูค่ามาที่ GetName() ออกมาดูจะทำให้เป็นค่าว่างๆ ("") 
+// ดังนั้นจำเป็นต้องใส่ * เมื่อมีการ assignment value ดังด้านล่างนี้ (p *Person) มันจะไปเปลี่ยนข้อมูล ณ ตำแหน่งจริงนั้นๆใน memory
 func (p *Person) SetName(name string) {
 	p.name = name
 }
